8d52e3adf23beb00fc029c8815f0a5cf62c1a1ed
mdmrk <mariodavo.20@gmail.com>
2023-10-09 10:24:37 +0200

    fix: clamp emoji size

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,20 +1,17 @@
+import { clamp } from "@utils/math";
 import { Emoji as _Emoji } from "emoji-picker-react";
 
 function getEmojiUrl(unified: string) {
-  return `https://cdn.staticfile.org/emoji-datasource-apple/14.0.0/img/apple/64/${unified}.png`;
+  return `https://cdn.staticfile.org/emoji-datasource-apple/15.0.0/img/apple/64/${unified}.png`;
 }
 
 export default function Emoji(props: { emoji: string; size?: number }) {
   const unified = [...props.emoji]
     .map(e => e.codePointAt(0)!.toString(16))
     .join(`-`);
+  const size = clamp(props.size ?? 18, 1, 64);
 
   return (
-    <_Emoji
-      lazyLoad
-      unified={unified}
-      getEmojiUrl={getEmojiUrl}
-      size={props.size ?? 18}
-    />
+    <_Emoji lazyLoad unified={unified} getEmojiUrl={getEmojiUrl} size={size} />
   );
 }
